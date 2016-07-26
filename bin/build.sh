4f0258fd98f12a710b00cbfd03a678a9c2a6cb6c
Christian B <chriss@multi-byte.de>
2016-07-26 18:37:47 +0200

    added gitignore file, moved install.sh to bin/build.sh, updated submodules, minor changes in Dockerfiles

---
+++ bin/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# read full path of parent directory
+DIR="$( readlink -f $(dirname $(dirname "${BASH_SOURCE[0]}")))"
+DOCKER=${DIR}/docker/volumes/cmangos
+
+mkdir -p ${DIR}/build
+mkdir -p ${DOCKER}
+
+cd ${DIR}/build
+cmake ../cmangos -DCMAKE_INSTALL_PREFIX=${DOCKER}
+
+make -j5
+make install
