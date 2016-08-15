012080560056b06cb64a79e6aadc9dc7a968f37e
Christian B <chriss@multi-byte.de>
2016-08-15 14:46:54 +0200

    modified params of docker call
    added --rm to docker command to remove mysql docker container after use

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -3,4 +3,4 @@
 NET=docker_default
 DB=percona
 
-docker run --net=$NET --link $DB -it percona /usr/bin/mysql -u root -p -h $DB
+docker run --rm --net=$NET --link $DB -it percona /usr/bin/mysql -u root -p -h $DB
